514be9b658b51e0bf559b2b035819f20806fa661
Mikhail Chalyshev <chalyshevma@gmail.com>
2020-08-14 15:42:40 +0500

    add project

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets'
+  },
+  autoplay: {
+    delay: 5000,
+  },
+})
